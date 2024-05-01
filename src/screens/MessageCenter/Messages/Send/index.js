952df3690dcdd52e2086545c840c758637640789
al-Reza89 <alialreza98@gmail.com>
2024-05-02 01:21:24 +0600

    send button

--- src/screens/MessageCenter/Messages/Send/index.js
+++ src/screens/MessageCenter/Messages/Send/index.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import cn from "classnames";
 import styles from "./Send.module.sass";
 import Icon from "../../../../components/Icon";
 import Smile from "../../../../components/Smile";
 
 const Send = () => {
+  const [input, setInput] = useState("");
+  const [textareaHeight, setTextareaHeight] = useState("50px");
+
+  const maxTextareaHeight = "100px"; // Change as per your requirement
+
+  const handleTextareaChange = (e) => {
+    setInput(e.target.value);
+    const height = e.target.scrollHeight;
+    const newHeight =
+      height > parseInt(maxTextareaHeight)
+        ? `${maxTextareaHeight}`
+        : `${height}px`;
+    setTextareaHeight(newHeight);
+  };
+
+  const scrollbarStyles = {
+    WebkitScrollbar: {
+      width: "8px",
+    },
+    WebkitScrollbarTrack: {
+      background: "#f1f1f1",
+    },
+    WebkitScrollbarThumb: {
+      background: "#888",
+      borderRadius: "4px",
+    },
+    WebkitScrollbarThumbHover: {
+      background: "#555",
+    },
+  };
+
   return (
     <div className={styles.send}>
       <div className={styles.file}>
@@ -13,12 +44,18 @@ const Send = () => {
       </div>
       <Smile className={styles.smile} up />
       <div className={styles.form}>
-        <input
+        <textarea
           className={styles.input}
-          type="text"
-          name="message"
-          placeholder="Message"
-          required
+          value={input}
+          onChange={handleTextareaChange}
+          style={{
+            height: textareaHeight,
+            maxHeight: maxTextareaHeight,
+            overflowY: "auto",
+            resize: "none",
+            ...scrollbarStyles,
+          }}
+          placeholder="Type your answer here"
         />
         <button className={cn("button-small", styles.button)}>Send</button>
       </div>
