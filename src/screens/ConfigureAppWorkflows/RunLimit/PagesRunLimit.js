359260417730d6fd7b260b8d3b4dfb29646de1ad
al-Reza89 <alialreza98@gmail.com>
2024-05-04 16:13:05 +0600

    tags

--- src/screens/ConfigureAppWorkflows/RunLimit/PagesRunLimit.js
+++ src/screens/ConfigureAppWorkflows/RunLimit/PagesRunLimit.js
@@ -4,8 +4,46 @@ import styles from "./RunLimit.module.sass";
 // import Modal from "../../../components/Modal";
 import TextInput from "../../../components/TextInput";
 import ConfigureModal from "../../../components/ConfigureModal";
+import { WithContext as ReactTags } from "react-tag-input";
 
 const PagesRunLimit = ({ onClose, open, item }) => {
+  const KeyCodes = {
+    comma: 188,
+    enter: 13,
+  };
+  const delimiters = [KeyCodes.comma, KeyCodes.enter];
+  const [tags, setTags] = useState([{ id: "Geometry", text: "Geometry" }]);
+  const handleDelete = (i) => {
+    setTags(tags.filter((tag, index) => index !== i));
+  };
+
+  const handleAddition = (tag) => {
+    setTags([...tags, tag]);
+  };
+
+  const handleDrag = (tag, currPos, newPos) => {
+    const newTags = [...tags].slice();
+
+    newTags.splice(currPos, 1);
+    newTags.splice(newPos, 0, tag);
+
+    setTags(newTags);
+  };
+
+  const handleTagClick = (index) => {
+    console.log("The tag at index " + index + " was clicked");
+  };
+
+  const onClearAll = () => {
+    setTags([]);
+  };
+
+  const onTagUpdate = (i, newTag) => {
+    const updatedTags = tags.slice();
+    updatedTags.splice(i, 1, newTag);
+    setTags(updatedTags);
+  };
+
   const [isActive, setIsActive] = useState(false);
 
   const toggleState = () => {
@@ -117,7 +155,33 @@ const PagesRunLimit = ({ onClose, open, item }) => {
               </div>
             </div>
           </div>
-          <div>button</div>
+          <div className={styles.tags}>
+            <ReactTags
+              handleDelete={handleDelete}
+              handleAddition={handleAddition}
+              handleDrag={handleDrag}
+              delimiters={delimiters}
+              handleTagClick={handleTagClick}
+              onClearAll={onClearAll}
+              onTagUpdate={onTagUpdate}
+              suggestions={[{ id: "1", text: "Geometry" }]}
+              placeholder="Enter tags to describe your item"
+              minQueryLength={2}
+              maxLength={20}
+              autofocus={false}
+              allowDeleteFromEmptyInput={true}
+              autocomplete={true}
+              readOnly={false}
+              allowUnique={true}
+              allowDragDrop={true}
+              inline={true}
+              inputFieldPosition="inline"
+              allowAdditionFromPaste={true}
+              editable={true}
+              clearAll={true}
+              tags={tags}
+            />
+          </div>
         </div>
 
         <div className={styles.btns}>
