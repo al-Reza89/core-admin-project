8a681f95b6a145bd81c4488a922dded9846fd795
al-Reza89 <alialreza98@gmail.com>
2024-05-04 08:28:25 +0600

    color picker

--- src/screens/ConfigureAppWorkflows/Table/Row/Settings.js
+++ src/screens/ConfigureAppWorkflows/Table/Row/Settings.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Row.module.sass";
 import Control from "../../../../components/Control";
 import cn from "classnames";
+import { SketchPicker } from "react-color";
 
 const Settings = ({
   item,
@@ -11,6 +12,38 @@ const Settings = ({
   setVisibleActions,
   options,
 }) => {
+  // State and functions for button1
+  const [displayColorPicker1, setDisplayColorPicker1] = useState(false);
+  const [color1, setColor1] = useState("#00ff00"); // Initial color for button1
+
+  const handleClick1 = () => {
+    setDisplayColorPicker1(!displayColorPicker1);
+  };
+
+  const handleClose1 = () => {
+    setDisplayColorPicker1(false);
+  };
+
+  const handleChange1 = (color) => {
+    setColor1(color.hex);
+  };
+
+  // State and functions for button2
+  const [displayColorPicker2, setDisplayColorPicker2] = useState(false);
+  const [color2, setColor2] = useState("blue"); // Initial color for button2
+
+  const handleClick2 = () => {
+    setDisplayColorPicker2(!displayColorPicker2);
+  };
+
+  const handleClose2 = () => {
+    setDisplayColorPicker2(false);
+  };
+
+  const handleChange2 = (color) => {
+    setColor2(color.hex);
+  };
+
   return (
     <div className={styles.row}>
       <div className={styles.col}>
@@ -43,24 +76,83 @@ const Settings = ({
               >
                 <button
                   style={{
+                    display: "flex",
+                    justifyContent: "center",
                     maxWidth: "200px",
-                    width: "90px",
+                    width: "110px",
                   }}
                   className={cn(styles.head)}
                 >
                   <div className={styles.value}>{item.button1}</div>
-                  {/* color box */}
+                  <div
+                    style={{
+                      width: "15px",
+                      height: "15px",
+                      backgroundColor: color1,
+                      marginLeft: "10px", // Adjust margin as needed
+                      cursor: "pointer",
+                    }}
+                    onMouseEnter={handleClick1}
+                  ></div>
+                  {displayColorPicker1 && (
+                    <div style={{ position: "absolute", zIndex: "2" }}>
+                      <div
+                        style={{
+                          position: "fixed",
+                          top: "0px",
+                          right: "0px",
+                          bottom: "0px",
+                          left: "0px",
+                        }}
+                        onClick={handleClose1}
+                      />
+                      <SketchPicker color={color1} onChange={handleChange1} />
+                    </div>
+                  )}
                 </button>
               </div>
-              <div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
                 <button
                   style={{
+                    display: "flex",
+                    justifyContent: "center",
                     maxWidth: "200px",
-                    width: "90px",
+                    width: "110px",
                   }}
                   className={cn(styles.head)}
                 >
                   <div className={styles.value}>{item.button2}</div>
+                  <div
+                    style={{
+                      width: "15px",
+                      height: "15px",
+                      backgroundColor: color2,
+                      marginLeft: "10px", // Adjust margin as needed
+                      cursor: "pointer",
+                    }}
+                    onMouseEnter={handleClick2}
+                  ></div>
+                  {displayColorPicker2 && (
+                    <div style={{ position: "absolute", zIndex: "2" }}>
+                      <div
+                        style={{
+                          position: "fixed",
+                          top: "0px",
+                          right: "0px",
+                          bottom: "0px",
+                          left: "0px",
+                        }}
+                        onClick={handleClose2}
+                      />
+                      <SketchPicker color={color2} onChange={handleChange2} />
+                    </div>
+                  )}
                 </button>
               </div>
             </div>
